862630bbf8916f63f81b059bc46b624d8c57c174
AutoViML <rsesha2001@yahoo.com>
2021-07-13 08:19:13 -0400

    corrected mistakes in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="deep_autoviml",
-    version="0.0.53",
+    version="0.0.54",
     author="Ram Seshadri",
     # author_email="author@example.com",
     description="Automatically Build Deep Learning Models and Pipelines fast!",
