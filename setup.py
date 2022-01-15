ce8b87e79f56c87cc008c128a6cfecb230df7b14
Sugato Ray <sugatoray@users.noreply.github.com>
2022-01-14 21:50:02 -0600

    setting version: `v0.0.78.dev2`

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="deep_autoviml",
-    version="0.0.78.dev1",
+    version="0.0.78.dev2",
     author="Ram Seshadri",
     # author_email="author@example.com",
     description="Automatically Build Deep Learning Models and Pipelines fast!",
